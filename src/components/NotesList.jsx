b0ae5cfad0bffe45c6b69f5f2d97c031fc283d2f
rfsyhb <rafisyihab3@gmail.com>
2024-03-27 10:18:40 +0700

    add NotesList.jsx

---
+++ src/components/NotesList.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import NoteItem from "./NoteItem";
+import PropTypes from "prop-types";
+
+function NotesList({ notes }) {
+  if (!notes.length) {
+    return (
+      <section className="note-list-empty">
+        <p>There is no note added!</p>
+      </section>
+    );
+  }
+
+  return (
+    <section className="notes-list">
+      {notes.map((note) => {
+        <NoteItem key={note.id} {...note} />;
+      })}
+    </section>
+  );
+}
+
+NotesList.propTypes = {
+  notes: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+export default NotesList;
