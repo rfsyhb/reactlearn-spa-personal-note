ba445d901085806f7af96e7d1115fd6e655549e9
rfsyhb <rafisyihab3@gmail.com>
2024-03-27 09:39:07 +0700

    add comp Navigation using Link

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Navigation() {
+  return (
+    <nav className="navigation">
+      <ul>
+        <li>
+          <Link to="/">Unarchived</Link>
+        </li>
+        <li>
+          <Link to="/archived">Archived</Link>
+        </li>
+        <li>
+          <Link to="/add">Add</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navigation;
