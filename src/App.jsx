5dc3a5a6c0d762a35dcf62985fa22ee7f7f1e927
rfsyhb <rafisyihab3@gmail.com>
2024-03-27 09:39:42 +0700

    add Routes and Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
 import React from "react";
+import Navigation from "./components/Navigation";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="app-container">
       <header>
         <h1>Notes App</h1>
-        <nav className="navigation">
-          <h2>Arsip</h2>
-          {/* <Navigation /> */}
-        </nav>
+        <Navigation />
       </header>
       <main>
-
+        <Routes>
+          <Route path="/" element={<p>Ini homepage</p>} />
+          <Route path="/archived" element={<p>Ini archived page</p>} />
+          <Route path="/add" element={<p>Ini add new note page</p>} />
+          <Route path="*" element={<p>Error 404: Page not found</p>} />
+        </Routes>
       </main>
     </div>
   );
